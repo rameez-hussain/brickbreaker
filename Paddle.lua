14411adff614c66d6cb2c2960d95e3f7e58d391f
AbdulRafay <abrarrafau96@gmail.com>
2019-07-06 21:58:27 +0500

    6 july

---
+++ Paddle.lua
@@ -0,0 +1,31 @@
+Paddle = Class{}
+
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dx = 0
+end
+
+function Paddle:update(dt)
+    -- math.max here ensures that we're the greater of 0 or the player's
+    -- current calculated Y position when pressing up so that we don't
+    -- go into the negatives; the movement calculation is simply our
+    -- previously-defined paddle speed scaled by dt
+    if self.dx < 0 then
+        self.x = math.max(0, self.x + self.dx * dt)
+    -- similar to before, this time we use math.min to ensure we don't
+    -- go any farther than the bottom of the screen minus the paddle's
+    -- height (or else it will go partially below, since position is
+    -- based on its top left corner)
+    else
+        self.x = math.min(VIRTUAL_WIDTH - self.width, self.x + self.dx * dt)
+    end
+end
+
+
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
+
--- main.lua
+++ main.lua
@@ -1,28 +1,159 @@
+-- IMPORTS
 
 push = require 'push'
 
+Class = require 'class'
+
+require 'Paddle'
+require 'Bricks'
+require 'Ball'
+
+-- WINDOW SIZE
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+lastWidth = 20 
+local background = love.graphics.newImage('background.png')
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
-VIRTUAL_WIDTH = 550
-VIRTUAL_HEIGHT = 361
+local bricksl1 = {}
+local bricksl2 = {}
+local bricksl3 = {}
+
+PADDLE_SPEED = 200
 
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+for i = 1,5
+do 
+if i > 1 then
+  lastWidth = bricksl1[i-1].x + 60
+  end
+table.insert(bricksl1, Bricks(lastWidth,(VIRTUAL_HEIGHT/2 - 10), 50, 5))
+
+ end
+    
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
+	player = Paddle(10, VIRTUAL_HEIGHT-20, 20, 5)
+
+ball = Ball(player.x/2 -2, player.y + 5, 4, 4)
+
+flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
+
+ push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
-        vsync = true
+        resizable = true,
+        vsync = true,
+           canvas = false
     })
 end
-  
- function love.draw()
-    
-    push:apply('start')
 
-    
-    love.graphics.printf('Brick breaker!', 0, 0, VIRTUAL_WIDTH, 'center')
+function love.keypressed(key)
+	if key == 'escape' then
+        -- function LÖVE gives us to terminate application
+        love.event.quit()
+    -- if we press enter during the start state of the game, we'll go into play mode
+    -- during play mode, the ball will move in a random direction
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
 
-    
-    push:apply('end')
+            -- ball's new reset method
+            ball:reset()
+        end
+    end
+
+end
+
+function love.update(dt)
+
+for i = 1,5 do
+ if bricksl1[i]:collides(ball) then
+   love.graphics.print("Collide")
+   
+   ball.dy = ball.dy
+            ball.y = bricksl1[i].y+bricksl1[i].height
+
+            -- keep velocity going in the same direction, but randomize it
+            if ball.dy < 0 then
+                ball.dy = math.random(100, 150)
+            else
+                ball.dy = -math.random(100, 150)
+            end
+   
+   end
+ 
+  end
+ 
+ if ball:collides(player) then
+            ball.dy = ball.dy
+            ball.y = player.y - 5
+
+            -- keep velocity going in the same direction, but randomize it
+            if ball.dy < 0 then
+                ball.dy = math.random(100, 150)
+            else
+                ball.dy = -math.random(100, 150)
+            end
+        end
+
+             -- detect upper and lower screen boundary collision and reverse if collided
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+
+
+           if ball.x <= 0 then
+            ball.x = 0
+            ball.dx = -ball.dx
+        end
+
+
+              -- -4 to account for the ball's size
+        
+
+        -- -4 to account for the ball's size
+        if ball.x >= VIRTUAL_WIDTH - 4 then
+            ball.x = VIRTUAL_WIDTH - 4
+            ball.dx = -ball.dx
+        end
+
+
+
+	if love.keyboard.isDown('right') then
+
+player.dx = PADDLE_SPEED 
+
+elseif love.keyboard.isDown('left') then
+	player.dx = -PADDLE_SPEED
+
+else
+	 player.dx = 0
+end
+
+ ball:update(dt)
+	player:update(dt)
+end
+
+function love.draw()
+push:start()
+love.graphics.draw(background, 0, 0)
+	    love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+         for k, brick in pairs(bricksl1) do
+           if brick.isCollide == false then
+        brick:render()
+        end
+        
+    end
+	    player:render()
+	    ball:render()
+push:finish()
 end
